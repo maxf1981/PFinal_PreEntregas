d08bd7415539cc988e51e298fb4281e900f6285b
maxf1981 <maxfarias81@gmail.com>
2023-11-08 09:01:45 -0300

    1er cambio

--- PreEntrega1.js
+++ PreEntrega1.js
@@ -1,42 +1,60 @@
 class ProductManager {
-    constructor () {
-        this.products = [];
+    constructor() {
+      this.products = [];
     }
-
-    getProducts () {
-        return this.products;
+  
+    getProducts() {
+      return this.products;
     }
-
-    addProduct (product) {
-        if (this.products.length === 0) {
-            product.id = 1;
-        } else {
-            product.id = this.products[this.products.length - 1].id + 1
-        }
-
-        this.products.push(product);
-        
-    }
-
-    
-
-    
-}
-
-class Product {
-    constructor(
+  
+    addProduct(productInfo) {
+      const productInfo = {title, description, price, thumbnail, code, stock};
+      if (this.products((product) => product.code === code)) {
+        console.log("El código del producto ya existe.");
+      }
+      const id = this.generarId();
+      const newProduct = {
+        id,
         title,
         description,
         price,
         thumbnail,
         code,
-        stock
-    ) {
-        this.title = title;
-        this.description = description;
-        this.price = price;
-        this.thumbnail = thumbnail;
-        this.code = code;
-        this.stock = stock;
+        stock,
+      };
+      this.products.push(newProduct);
+      return newProduct;
     }
-}
+  
+    getProductById(id) {
+      const product = this.products.find((product) => product.id === id);
+      if (!product) {
+        console.log("Producto no encontrado.");
+      }
+      return product;
+    }
+  
+    
+  }
+  
+  // Crear una instancia de ProductManager
+  const productManager = new ProductManager();
+  
+  // Obtener productos 
+  console.log(productManager.getProducts());
+  
+  // Testing
+  // Agregar un producto
+  const productData = {
+    title: "producto prueba",
+    description: "Este es un producto prueba",
+    price: 200,
+    thumbnail: "Sin imagen",
+    code: "a1",
+    stock: 25,
+  };
+  
+  const nuevoProducto = productManager.addProduct(productData);
+  console.log("Producto agregado:", nuevoProducto);
+  
+  
