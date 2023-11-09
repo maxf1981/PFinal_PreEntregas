9d9919c0c2b54b1f685ba31e5f8fac78561d0bcc
maxf1981 <maxfarias81@gmail.com>
2023-11-08 23:47:20 -0300

    2da Pre entrega

---
+++ PreEntrega2.js
@@ -0,0 +1,60 @@
+class ProductManager {
+    constructor() {
+      this.products = [];
+    }
+  
+    getProducts() {
+      return this.products;
+    }
+  
+    addProduct(productInfo) {
+      const productInfo = {title, description, price, thumbnail, code, stock};
+      if (this.products((product) => product.code === code)) {
+        console.log("El código del producto ya existe.");
+      }
+      const id = this.generarId();
+      const newProduct = {
+        id,
+        title,
+        description,
+        price,
+        thumbnail,
+        code,
+        stock,
+      };
+      this.products.push(newProduct);
+      return newProduct;
+    }
+  
+    getProductById(id) {
+      const product = this.products.find((product) => product.id === id);
+      if (!product) {
+        console.log("Producto no encontrado.");
+      }
+      return product;
+    }
+  
+    
+  }
+  
+  // Crear una instancia de ProductManager
+  const productManager = new ProductManager();
+  
+  // Obtener productos 
+  console.log(productManager.getProducts());
+  
+  // Testing
+  // Agregar un producto
+  const productInfo = {
+    title: "producto prueba",
+    description: "Este es un producto prueba",
+    price: 200,
+    thumbnail: "Sin imagen",
+    code: "a1",
+    stock: 25,
+  };
+  
+  const nuevoProducto = productManager.addProduct(productInfo);
+  console.log("Producto agregado:", nuevoProducto);
+  
+  
